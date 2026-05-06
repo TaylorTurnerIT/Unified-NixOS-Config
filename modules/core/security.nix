5149fa6bd6426eb4d2562c4ae841a338667525f4
Taylor <taylort3450@proton.me>
2026-05-06 17:26:27 -0500

    feat: Remove duplicate configuration for reverse path filtering

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -23,8 +23,6 @@
   # Kernel hardening
   boot.kernel.sysctl = {
     "kernel.dmesg_restrict"              = 1;
-    "net.ipv4.conf.all.rp_filter"        = 1;
-    "net.ipv4.conf.default.rp_filter"    = 1;
     "net.ipv4.tcp_syncookies"            = 1;
     "net.ipv6.conf.all.accept_redirects" = 0;
   };
